af15cc9533e016c45ec5c21c852d08af8208fcfd
nathanael <vieira_nathanael@hotmail.com>
2022-07-15 17:23:37 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,24 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg'; //Apelido icon from => (Voltando pasta anterion ../)
+import { BASE_URL } from '../../utils/request';
 import './style.css'; //Link com css na mesma pasta => ./
 
+type Props = {
+	saleId: number;
 
-function NotificationButton() {
+}
+
+
+function handleClick(id: number) {
+	axios(`${BASE_URL}/sales/${id}/notification`)
+		.then(response => {
+			console.log("Sucesso nathan!");
+	})
+}
+
+function NotificationButton({saleId}: Props) {
   return (
-	  <div className="dsmeta-red-btn">
+	  <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
 		  <img src={icon} alt="Notificar" />
 	  </div>
   )
